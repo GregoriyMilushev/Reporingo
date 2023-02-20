541e020472b35dbd1727787bf4184f644fd67354
EmilArnaudov <emil.arnaudov.ecom@gmail.com>
2023-02-20 10:01:23 +0200

    remove croping

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import 'react-native-url-polyfill/auto';
-import ImagePickerExample from './app/components/ImagePicker';
 import CreateReportScreen from './app/screens/CreateReportScreen';
 
 export default function App() {
--- app/components/ImagePicker.jsx
+++ app/components/ImagePicker.jsx
@@ -9,8 +9,6 @@ const SelectImage = () => {
   const pickImageFromGallery = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
       quality: 1,
     });
 
@@ -21,8 +19,6 @@ const SelectImage = () => {
 
   const takePhoto = async () => {
     let result = await ImagePicker.launchCameraAsync({
-      allowsEditing: true,
-      aspect: [4, 3],
       quality: 1,
     });
 
