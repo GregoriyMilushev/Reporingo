98fda6a96bc8b8ba985b0f1f5a895058d5f68f0c
Aleksandar Radev <alexander.radev.96@gmail.com>
2023-02-25 17:36:46 +0200

    add format date helper function

---
+++ app/assets/helpers.js
@@ -0,0 +1,11 @@
+export function formatDateForUI(date) {
+  const formattedDate = 'Invalid Date';
+  try {
+    date = new Date(date);
+    const day = date.getDate().toString().padStart(2, '0'); // Get day and add leading zero if necessary
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Get month and add leading zero if necessary
+    const year = date.getFullYear().toString(); // Get year
+    formattedDate = `${day}/${month}/${year}`;
+  } catch {}
+  return formattedDate;
+}
